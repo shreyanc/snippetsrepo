523c4196222c8565d15eb24aa4aa52e95de0c73a
Shreyan Chowdhury <shreyan0311@gmail.com>
2023-05-18 17:26:22 +0200

    add EasyDict to utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+class EasyDict(dict):
+    """
+    Allows you to access and modify dictionary keys using dot notation instead of the usual square bracket notation.
+    """
+    def __getattr__(self, name: str) -> Any:
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        self[name] = value
+
+    def __delattr__(self, name: str) -> None:
+        del self[name]
